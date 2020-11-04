2fbb904e105b598ca7462ba82cda0dddff907a56
Jonathan Kevin <jonathan.kevinbp@hotmail.com>
2020-11-04 01:17:35 -0300

    deliveryman and avatar

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import authMiddleware from './app/middleware/auth';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 const routes = new Router();
 
@@ -10,6 +11,11 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
-routes.put('/recipients', RecipientController.update);
+routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliveryman', DeliverymanController.index);
+routes.post('/deliveryman', DeliverymanController.store);
+routes.put('/deliveryman/:id', DeliverymanController.update);
+routes.delete('/deliveryman/:id', DeliverymanController.delete);
 
 export default routes;
